12a95c260ee1b1256d43711501245e0d9d26e65a
Navtej Singh <navtejsingh15032011@gmail.com>
2026-01-01 22:19:32 +0530

    Add files via upload

---
+++ installation.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+print("--NEED TO INSTALL SOME LINUX PACKAGES AND PYTHON3 PACKAGES--")
+print("Installation will be starting in 5 seconds ")
+
+time.sleep(5)
+
+os.system("sudo apt update")
+os.system("sudo apt install espeak espeak-ng ffmpeg libespeak1 alsa-utils -y")
+os.system("pip install pyttsx3 --break-system-packages")
+os.system("pip install google-generativeai --break-system-packages")
+os.system("pip install google-genai")
+
+print("DONE........... NOW NOW YOU CAN START RUNNING ai.py")
+user = input("NEED MORE GITHUB TOOLS?    [Yes/No]")
+if user.lower() in ["yes", "y"]:
+	os.system("git clone https://github.com/Navtej-Singh-1503/Android-viruses")
+	os.system("git clone https://github.com/Navtej-Singh-1503/Tiny-Platformer-Game")
+	print("DONE... MORE TOOLS ARE INSTALLED THANKS!")
+elif user.lower() in ["no", "n"]:
+	print("OKK!!")
+else:
+	print("invaild input")
+	os.system("installation.py")
+
+
+
