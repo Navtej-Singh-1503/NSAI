03bf3ed602cb0c938ff447f1324539311fd9c8c3
Navtej Singh <navtejsingh15032011@gmail.com>
2026-01-02 11:19:50 +0530

    installation.py

--- installation.py
+++ installation.py
@@ -1,6 +1,9 @@
+from brand import intro
 import os
 import time
 
+print(intro)
+time.sleep(1)
 print("--NEED TO INSTALL SOME LINUX PACKAGES AND PYTHON3 PACKAGES--")
 print("Installation will be starting in 5 seconds ")
 
@@ -13,6 +16,10 @@ os.system("pip install google-generativeai --break-system-packages")
 os.system("pip install google-genai")
 
 print("DONE........... NOW NOW YOU CAN START RUNNING ai.py")
+print("Need Gemini API Key to run ai.py")
+print("If you don't have you can get it on ")
+key = input("Enter you Google gemini API Key>> ")
+
 user = input("NEED MORE GITHUB TOOLS?    [Yes/No]")
 if user.lower() in ["yes", "y"]:
 	os.system("git clone https://github.com/Navtej-Singh-1503/Android-viruses")
