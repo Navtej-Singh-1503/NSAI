d3fb951f7e47f9b8f8db584bbf78c8036149e7ee
Navtej Singh <navtejsingh15032011@gmail.com>
2026-01-02 21:24:24 +0530

    Update installation.py

--- installation.py
+++ installation.py
@@ -2,10 +2,15 @@ from brand import intro
 import os
 import time
 
-print(intro)
+RED = "\033[1;31m"
+GREEN = "\033[0;32m"
+RESET = "\033[0m"
+os.system('clear')
+
+print(RED + intro)
 time.sleep(1)
-print("--NEED TO INSTALL SOME LINUX PACKAGES AND PYTHON3 PACKAGES--")
-print("Installation will be starting in 5 seconds ")
+print(GREEN+"--NEED TO INSTALL SOME LINUX PACKAGES AND PYTHON3 PACKAGES--")
+print("Installation will be starting in 5 seconds "+RESET)
 
 time.sleep(5)
 
@@ -15,7 +20,7 @@ os.system("pip install pyttsx3 --break-system-packages")
 os.system("pip install google-generativeai --break-system-packages")
 os.system("pip install google-genai --break-system-packages")
 
-print("DONE........... NOW NOW YOU CAN START RUNNING ai.py")
+print(GREEN+"DONE........... NOW NOW YOU CAN START RUNNING ai.py")
 print("Need Gemini API Key to run ai.py")
 print("If you don't have you can get it on https://aistudio.google.com/api-keys")
 key = input("Enter you Google gemini API Key>> ")
@@ -23,7 +28,7 @@ key = input("Enter you Google gemini API Key>> ")
 with open("api.py", "w") as f:
     f.write('apikey = "'+key+'"')
 
-print("api.py file created successfully!")
+print("api.py file created successfully!"+RESET)
 
 user = input("NEED MORE GITHUB TOOLS?    [Yes/No]")
 if user.lower() in ["yes", "y"]:
