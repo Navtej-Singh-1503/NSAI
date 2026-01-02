637cf1e6af36a846301a384003bee9958b47e1ce
Navtej Singh <navtejsingh15032011@gmail.com>
2026-01-02 22:56:29 +0530

    Update DATA.py

--- DATA.py
+++ DATA.py
@@ -1,3 +1,17 @@
+'''
+CREATED BY Navtej-Singh-1503
+© 2025 Navtej Singh Saggar
+Educational use only
+
+01/01/2026
+
+Version - 0.5.3
+
+mail - navtejsingh15032011@gmail.com
+
+'''
+
+
 SYSTEM_PROMPT = """
 You are NavAI, a smart personal assistant created by Navtej Singh Saggar.
 
